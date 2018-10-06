613f5263c8717d4747b5161a7b018e2fcbb1af68
dlobba <diego.lobba@studenti.unitn.it>
2018-10-06 08:11:50 +0200

    added sender id to the message for easier logging

--- ds2lab/src/main/java/com/projects/gillo/epidemic/EpidemicMsg.java
+++ ds2lab/src/main/java/com/projects/gillo/epidemic/EpidemicMsg.java
@@ -5,9 +5,11 @@ import java.io.Serializable;
 public abstract class EpidemicMsg implements Serializable {
 	
 	private long timestamp;
+	protected int sid;
 	
-	public EpidemicMsg(long timestamp) {
+	public EpidemicMsg(long timestamp, int sid) {
 		this.timestamp = timestamp;
+		this.sid = sid;
 	}
 
 	public long getTimestamp() {

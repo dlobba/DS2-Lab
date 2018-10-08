212ac74eb56e7ba67f9d8ebee6dc9dcbd390db1b
dlobba <diego.lobba@studenti.unitn.it>
2018-10-08 22:56:18 +0200

    refactored logic with renamed variables

--- ds2lab/src/main/java/com/projects/gillo/adaptive/gossip/EpidemicActor.java
+++ ds2lab/src/main/java/com/projects/gillo/adaptive/gossip/EpidemicActor.java
@@ -19,37 +19,52 @@ public class EpidemicActor extends AbstractActor {
 	// min events number within events
 	public static final int MIN_SIZE = 5;
 	
+	// not used yet TODO
 	private static int  H = 7;
 	private static int  L = 5;
 	private static double rH = 0.05;
 	private static double rL = 0.05;
 	private static double W = 0.5;
 	private static double alpha = 0.8;
-	private long k;
+	// end not used yet TODO: DO IT
 	
-	private int f = 1;
 	
-	int id;
-	final long timeoutPeriod = 100;
-	final long delta = 2;
-	final long period;
+	private static long maxAge = 5; // age after which an event becomes old
+									// and must be discarded, it's the **k** in the alg
+	private static int numberInformed = 1; // it's the f in the algorithm, the amount of
+										   // participants contacted in the multicast
+	
+	final long T = 1000; // gossip update timeout (ms)
+	final long S = 1000; // minBuff update timeout (ms)
+	
 	
 	private List<Event> events;
 	private int eventIdGen;
+	private static final int UPPER_MAX_EVENTS = 10;
+	private final int maxEvents; // this is |events|_m in the paper
 	
-	private long bufferSize;
-	private long minBufferSize;
-	
+	final long delta = 2; // periods window (in periods)
+	private long period; // current period, it's the **s** in the paper
+	private long minBuff;
+	private MinBuffList minBuffSerie;	
+
+	int id;
 	List<ActorRef> actors;
-	
+
 	public EpidemicActor(int id) {
 		this.id = id;
-		events = new ArrayList<>();
-		eventIdGen = 0;
-		bufferSize = new Random().nextInt(RANGE) + MIN_SIZE;
-		minBufferSize = bufferSize;
 		actors = new ArrayList<>();
-		this.period = this.delta;
+		eventIdGen = 0;
+		
+		this.maxEvents = new Random().nextInt(UPPER_MAX_EVENTS) + 1;
+		
+		this.events = new ArrayList<>();
+		this.period = this.delta - 1;
+		this.minBuff = this.maxEvents; // default value;
+		this.minBuffSerie = new MinBuffList(this.delta);
+		for (long i = 0; i < this.delta; i++) {
+			this.minBuffSerie.add(new MinBuff(i, this.maxEvents));
+		}
 	}
 	
 	static Props props(int id) {
@@ -57,86 +72,47 @@ public class EpidemicActor extends AbstractActor {
 				() -> new EpidemicActor(id));
 	}
 	
-	void sendTimeoutMsg() {
-		this.getContext()
-        .getSystem()
-        .scheduler()
-        .scheduleOnce(Duration.create(this.timeoutPeriod,
-                TimeUnit.MILLISECONDS),
-                this.getSelf(),
-                new TimeoutMsg(),
-                getContext().system().dispatcher(),
-                this.getSelf());
-	}
-	
 	void onStartMessage(StartMessage msg) {
 		this.actors = new ArrayList<ActorRef>(msg.getActors());
-		sendTimeoutMsg();
+		sendGossipTimeoutMsg();
+		sendMinBuffTimeoutMsg();
 	}
 	
-	void onInfectedMsg(InfectedMessage msg) {
+	void onBroadcast(BroadcastMsg msg) {
+		// TODO: missing implementation
 		Event event = new Event(this.eventIdGen, 
-				this.minBufferSize,
 				"mex" + this.eventIdGen,
 				this.id);
 		this.eventIdGen += 1;
 		this.events.add(event);
+		System.out.printf("P%d added message %s\n", this.id, event.toString());
 	}
 	
-	private long maxAge() {
-		long max = 0;
-		for (Event event : events) {
-			if (event.getAge() > max)
-				max = event.getAge();
-		}
-		return max;
-	}
-	
-	private Event getMaxEvent() {
-		long max = maxAge();
-		for (Event event : events) {
-			if (event.getAge() == max)
-				return event;
-		}
-		return null;
-	}
-	
-
-	void onTimeout(TimeoutMsg msg) {
+	void onGossipTimeout(GossipTimeoutMsg msg) {
 		for (Event event : events) {
 			event.setAge(event.getAge() + 1);
 		}
-		k = maxAge();
 		Iterator<Event> eventIter = events.iterator();
 		Event tempEvent = null;
 		while (eventIter.hasNext()) {
 			tempEvent = eventIter.next();
-			if (tempEvent.getAge() >= k)
+			if (tempEvent.getAge() > maxAge)
 				eventIter.remove();
 		}
-		//TODO: bufferSize vs minBufferSize
-		Gossip gossip = new Gossip(this.events, this.minBufferSize);
+		// at least the current period is in the serie
+		MinBuff currentMinBuff = this.minBuffSerie.getMinBuff(this.period);
+		Gossip gossip = new Gossip(this.events,
+				currentMinBuff.bufferSize,
+				this.period);
 		
 		List<ActorRef> tmpActors = new ArrayList<>(this.actors);
-		for (int i = 0; i < this.f; i++) {
+		for (int i = 0; i < numberInformed; i++) {
 			int rand = new Random().nextInt(this.actors.size());
 			ActorRef tmp = tmpActors.get(rand);
 			tmpActors.remove(rand);
 			// send gossip
 			tmp.tell(gossip, this.getSelf());
 		}
-	}
-	
-	private void deliver(Event event) {
-		System.out.printf("Delivered %s\n", event.toString());
-	}
-	
-	private int getEvent(int eid, int sid) {
-		for (Event event : events) {
-			if (event.getId() == eid && event.getSenderId() == sid)
-				return events.indexOf(event);
-		}
-		return -1;
 	}
 	
 	void onReceive(Gossip msg) {
@@ -150,7 +126,7 @@ public class EpidemicActor extends AbstractActor {
 			}
 			
 			int eventIndex = getEvent(event.getId(), event.getSenderId());
-			if (eventIndex > -1) throw new IllegalStateException();
+			if (eventIndex < 0) throw new IllegalStateException();
 			
 			Event tmpEvent = this.events.remove(eventIndex);
 			if (tmpEvent.getAge() < event.getAge()) {
@@ -158,20 +134,91 @@ public class EpidemicActor extends AbstractActor {
 			}
 			this.events.add(tmpEvent);
 			
-			while (this.events.size() > this.minBufferSize) {
+			while (this.events.size() > this.maxEvents) {
 				this.events.remove(getMaxEvent());
 			}
-			
 		}
+		
+		// update the minBuffer
+		MinBuff currentMinBuff = this.minBuffSerie.getMinBuff(this.period);
+		if (this.period == msg.getPeriod() &&
+				msg.getBufferSize() < currentMinBuff.bufferSize)
+			currentMinBuff.bufferSize = msg.getBufferSize();
+	}
+	
+	private void onMinBuffTimeout(MinBuffTimeoutMsg msg) {
+		synchronized (msg) {			
+			this.period += 1;
+			MinBuff mb = new MinBuff(this.period, this.maxEvents);
+			this.minBuffSerie.add(mb);
+			this.minBuff = this.minBuffSerie.getMinBufferSize();
+		}
+		System.out.printf("P%d %s\n", this.id, this.minBuffSerie.toString());
+		sendMinBuffTimeoutMsg();
+	}
+	
+	private void deliver(Event event) {
+		System.out.printf("P%d delivered %s\n", this.id, event.toString());
+	}
+	
+	private int getEvent(int eid, int sid) {
+		Iterator<Event> eventIter = this.events.iterator();
+		Event event;
+		int index = 0;
+		while (eventIter.hasNext()) {
+			event = eventIter.next();
+			if (event.getId() == eid && event.getSenderId() == sid)
+				return index;
+			index += 1;
+		}
+		return -1;
 	}
 
+	private Event getMaxEvent() {
+		long max = -1;
+		Event temp = null;
+		for (Event event : events) {
+			if (event.getAge() > max) {
+				temp = event;
+				max = event.getAge();
+			}
+		}
+		return temp;
+	}
+	
+	void sendGossipTimeoutMsg() {
+		this.getContext()
+        .getSystem()
+        .scheduler()
+        .scheduleOnce(Duration.create(this.T,
+                TimeUnit.MILLISECONDS),
+                this.getSelf(),
+                new GossipTimeoutMsg(),
+                getContext().system().dispatcher(),
+                this.getSelf());
+	}
+	
+	void sendMinBuffTimeoutMsg() {
+//		System.out.printf("P%d started minBuff timeout\n", this.id);
+		this.getContext()
+        .getSystem()
+        .scheduler()
+        .scheduleOnce(Duration.create(this.T,
+                TimeUnit.MILLISECONDS),
+                this.getSelf(),
+                new MinBuffTimeoutMsg(),
+                getContext().system().dispatcher(),
+                this.getSelf());
+	}
+	
 	@Override
 	public Receive createReceive() {
 		return receiveBuilder()
 				.match(StartMessage.class, this::onStartMessage)
-				.match(TimeoutMsg.class, this::onTimeout)
+				.match(GossipTimeoutMsg.class, this::onGossipTimeout)
+				.match(MinBuffTimeoutMsg.class, this::onMinBuffTimeout)
 				.match(Gossip.class, this::onReceive)
-				.match(InfectedMessage.class, this::onInfectedMsg)
+				.match(BroadcastMsg.class, this::onBroadcast)
 				.build();
 	}
 
--- ds2lab/src/main/java/com/projects/gillo/adaptive/gossip/EpidemicMain.java
+++ ds2lab/src/main/java/com/projects/gillo/adaptive/gossip/EpidemicMain.java
@@ -2,38 +2,53 @@ package com.projects.gillo.adaptive.gossip;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.projects.gillo.adaptive.gossip.messages.BroadcastMsg;
+import com.projects.gillo.adaptive.gossip.messages.StartMessage;
+
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 
 public class EpidemicMain {
 	
+	public static final int nparticipants = 5;
+	
 	public static void main(String[] args) {
 		ActorSystem system = ActorSystem.create("Epidemic");
-//		Set<ActorRef> actors = createActors(system, epiType, 5);
+		List<ActorRef> actors = new ArrayList<>();
 		
-//		StartMessage groupMsg = new StartMessage(actors);
-//		for (ActorRef actor : actors) {
-//			actor.tell(groupMsg, null);
-//		}
+		
+		for (int i = 0; i < nparticipants; i++) {
+			actors.add(system.actorOf(EpidemicActor
+					.props(i)
+					.withDispatcher("akka.actor.my-pinned-dispatcher"), "actor" + i));
+		}
+		
+		StartMessage groupMsg = new StartMessage(actors);
+		for (ActorRef actor : actors) {
+			actor.tell(groupMsg, null);
+		}
 		
 		// generate a series of update, giving
 		// 5s to the system to get up to date.
 		// note: it can happen that within this interval
 		// some node could not be able to receive the update
-//		for (int i = 0; i < 3; i++) {
-//			try {
-//				ActorRef infected = (ActorRef) actors
-//						.toArray()[new Random()
-//						           .nextInt(actors.size())];
-//				System.out.printf("Started infection %d\n", i);
-//				infected.tell(new InfectedMessage(new Random()
-//						.nextInt(Integer.MAX_VALUE)),
-//						null);
-//				Thread.sleep(5000);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		System.exit(0);
+		for (int i = 0; i < 3; i++) {
+			try {
+				ActorRef infected = (ActorRef) actors
+						.toArray()[new Random()
+						           .nextInt(actors.size())];
+				System.out.printf("Started infection %d\n", i);
+				infected.tell(new BroadcastMsg(), null);
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.exit(0);
 	}
 
 }
